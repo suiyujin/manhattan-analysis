44e69bcdd6317937def1d85f66226484607b5010
suiyujin <suiyujin04@gmail.com>
2016-02-13 23:40:18 +0900

    add logger messages

--- app/controllers/shiritoris_controller.rb
+++ app/controllers/shiritoris_controller.rb
@@ -6,10 +6,13 @@ class ShiritorisController < ApplicationController
   def analysis
     # TODO: csvファイルを作る
     csv_filenames = make_csv_filenames
+    logger.info("csv_filenames : #{csv_filenames}")
     data = make_array
 
     # csvファイルを保存する
     home_dir = `echo $HOME`.chomp
+    logger.info("home_dir : #{home_dir}")
+
     data.each_with_index do |csv_data, index|
       CSV.open("#{home_dir}/manhattan_csv/#{csv_filenames[index]}", 'w') do |csv_file|
         csv_data.each do |row|
@@ -21,17 +24,17 @@ class ShiritorisController < ApplicationController
     # Zinnia, Mecubで調べる
     result = `checkword #{csv_filenames.join(' ')}`
     word = result.match(/^[^\s]+/)[0]
-    p "word : #{word}"
+    logger.info("word : #{word}")
 
     if result =~ /\A0/
-      p "0 : 認識不可!"
+      logger.info("0 : 認識不可!")
       res_json = { status: 0, message: "認識不可！" }
     elsif word.size < WORD_SIZE
-      p "1 : 単語無し！"
+      logger.info("1 : 単語無し！")
       res_json = { status: 1, message: "単語無し！", word: word }
     else
       # TODO: しりとりチェック
-      p "3 : ok!"
+      logger.info("3 : ok!")
       res_json = { status: 3, message: "ok!", word: word }
     end
     render json: res_json
